82aade907cb4d3df6f2af9c07c599a2532776037
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-01 09:08:38 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/notifikasi/sms/gratika/NotifikasiGratikaApplication.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.notifikasi.sms.gratika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+
+@SpringBootApplication @EnableKafka
+public class NotifikasiGratikaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NotifikasiGratikaApplication.class, args);
+	}
+}
